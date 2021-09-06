ed1f16196cbe8d076a3b5e240bc9b7cd0b7c9ed1
PauloBaracho <89670070+PauloBaracho@users.noreply.github.com>
2021-09-06 19:58:50 -0300

    Mudei o LifeTime

--- sketch.js
+++ sketch.js
@@ -131,7 +131,7 @@ function gerarNuvens() {
     nuvem.velocityX = -3;
     
      //atribuir tempo de duração à variável
-    nuvem.lifetime = 220; 
+    nuvem.lifetime = 300; 
     
     //ajustando a profundidade
     nuvem.depth = trex.depth;
